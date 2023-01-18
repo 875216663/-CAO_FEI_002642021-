70ce82f8b101aa44e4ec13e7d9991df977991a84
FEI CAO <122421787+875216663@users.noreply.github.com>
2023-01-17 22:39:11 -0500

    Hello World
    1. Create a package called ‘Model’ 
    2. Create a Java File named HelloWorld.java inside the model package 
    3. Write a program using HelloWorld class which initializes 5 variables of different primitive data 
    types – (int, double, float, string, Boolean, char) 
    4. Initialize an integer array of size ‘n’  
    5. Display the above variables in the main() method of HelloWorld class 
    6. Execute/Run the HelloWorld.java class to check your output

---
+++ JavaApplication2/src/Model/HelloWorld.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author caofei
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        int x=1;
+	double y=2.5;
+	float z=2;
+	String p="love";
+	Boolean q=true;
+	char t='u';
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(q);
+		System.out.println(t);
+		System.out.println(p);
+        int[] arr=new int[5];//n=5
+        	System.out.println(arr[1]);
+        
+   
+   
+    }
+}
